ca485843ce303dc0bfc870611b2cb1aed9e86671
Tetiana <ttobin@protonmail.ch>
2021-07-02 20:48:49 +0100

    Update ingredients.js

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -81,9 +81,8 @@ const idlength = 8;
  *                 $ref: '#/components/schemas/Ingredient'
  *
  */
-router.get('/v1/', (req, res) => {
+router.get('/v1', (req, res) => {
   const ingredients = req.app.db.get('ingredients');
-
   res.send(ingredients);
 });
 
